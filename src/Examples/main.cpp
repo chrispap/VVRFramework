ceb474365ac08301d385f414c031d8b7e69fa11b
Chris Papapavlou (Debian 7) <chrispapapaulou@ece.upatras.gr>
2015-04-03 00:06:40 +0300

    Added GeoLib in src

--- src/Examples/main.cpp
+++ src/Examples/main.cpp
@@ -2,10 +2,12 @@
 #include "Ex_02_OrientationViewerScene.h"
 #include "Ex_03_PhysicsEngineScene.h"
 #include "Ex_04_ContourEditor.h"
+#include "Scene_ConvexHull.h"
 
 int main(int argc, char* argv[])
 {
     /* Example scenes using VVR Framework */
+    return vvr::mainLoop(argc, argv, new Scene_ConvexHull);
     return vvr::mainLoop(argc, argv, new Simple2DScene);
     return vvr::mainLoop(argc, argv, new OrientationViewerScene);
     return vvr::mainLoop(argc, argv, new PhysicsEngineScene);
---
+++ src/GeoLib/StdAfx.h
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------
+Copyright (C) GeoLib.
+This code is used under license from GeoLib (www.geolib.co.uk). This or
+any modified versions of this cannot be resold to any other party.
+---------------------------------------------------------------------------*/
+
+/**--------------------------------------------------------------------------<BR>
+\file StdAfx.h
+\brief File used for all commonly used includes within the library.
+<P>---------------------------------------------------------------------------*/
+
+#ifndef _GEOLIB_STDAFX_H 
+#define _GEOLIB_STDAFX_H
+
+#include <cassert>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include "Constants.h"
+
+using namespace std;
+
+class C2DBase;
+typedef std::vector<C2DBase*> C2DBaseData;
+
+#endif
