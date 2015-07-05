8bce6e099cc70195812d28a18d0694c155cd6095
Christos Papapavlou (VVRLab) <chrispapapaulou@ece.upatras.gr>
2015-07-05 20:48:02 +0300

    Remove installation dirs...

--- install-vs13-release/include/VVRScene/vvrscenedll.h
+++
@@ -1,9 +0,0 @@
-#ifdef _WIN32
-#	ifdef VVRScene_EXPORTS
-#		define VVRScene_API __declspec(dllexport)
-#	else
-#		define VVRScene_API  __declspec(dllimport)
-#	endif
-#else
-#	define VVRScene_API
-#endif // WIN32
