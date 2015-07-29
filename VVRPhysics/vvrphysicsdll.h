d7e7fa78366a6aaadd4fa49ea12165a7cdef9b71
Christos Papapavlou (VVRLab) <chrispapapaulou@ece.upatras.gr>
2015-07-29 19:40:15 +0300

    Debug libs

--- VVRPhysics/vvrphysicsdll.h
+++
@@ -1,9 +0,0 @@
-#ifdef _WIN32
-#	ifdef VVRPhysics_EXPORTS
-#		define VVRPhysics_API __declspec(dllexport)
-#	else
-#		define VVRPhysics_API  __declspec(dllimport)
-#	endif
-#else
-#	define VVRPhysics_API
-#endif // WIN32
