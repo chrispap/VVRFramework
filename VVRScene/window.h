20d7c01d447f14d7f9ec21017d3d8a3eae29b8c9
Chris Papapavlou [Win10@Work] <chrispapapaulou@ece.upatras.gr>
2016-03-30 15:24:22 +0300

    Tidy up window.h

--- VVRScene/window.h
+++ VVRScene/window.h
@@ -8,37 +8,42 @@
 
 namespace vvr {
 
-    class Window : public QMainWindow, private Ui::MainWindow
-    {
-        Q_OBJECT
+class Window : public QMainWindow, private Ui::MainWindow
+{
+    Q_OBJECT
 
-    public:
-        Window(vvr::Scene *scene);
+public:
+    Window(vvr::Scene *scene);
 
-    private slots:
-        void about();
-        void sliderMoved(int val);
+private slots:
+    void about();
+    void sliderMoved(int val);
 
-    signals:
-        void keyPressed(QKeyEvent *event);
+signals:
+    void keyPressed(QKeyEvent *event);
 
-    private:
-        virtual void keyPressEvent(QKeyEvent* event);
-        void createActions();
-        void createMenus();
-    private:
-        static void log_cout(const char* ptr, std::streamsize count, void*);
-        static void log_cerr(const char* ptr, std::streamsize count, void*);
+private:
+    virtual void keyPressEvent(QKeyEvent* event);
+    void createActions();
+    void createMenus();
 
-    private:
-        static QString aboutMessage;
-        QMenu *fileMenu, *helpMenu;
-        QAction *exitAct, *aboutAct;
-        GLWidget *glWidget;
-        Scene *scene;
-        StdRedirector<> *m_std_cout_logger;
-        StdRedirector<> *m_std_cerr_logger;
-    };
+private:
+    static void log_cout(const char* ptr, std::streamsize count, void*);
+    static void log_cerr(const char* ptr, std::streamsize count, void*);
+
+private:
+    static QString aboutMessage;
+
+private:
+    Scene *scene;
+    GLWidget *glWidget;
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+    QAction *exitAct;
+    QAction *aboutAct;
+    StdRedirector<> *m_std_cout_logger;
+    StdRedirector<> *m_std_cerr_logger;
+};
 
 }
 
